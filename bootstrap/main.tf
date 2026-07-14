4815050798517199f6ee86858149625718663346
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-07-14 16:55:30 +0100

    remove dynamodb resource

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -49,13 +49,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_state" {
-  name         = var.lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}

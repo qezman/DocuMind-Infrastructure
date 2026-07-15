f9d14214d0f3b9adb4df71c2e7992d327a914f54
qezman <holaryinka5050@gmail.com>
2026-07-15 23:26:51 +0100

    feat: EKS cluster module

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -30,3 +30,12 @@ module "vpc" {
   vpc_cidr           = var.vpc_cidr
   availability_zones = var.availability_zones
 }
+
+module "eks" {
+  source             = "../../modules/eks"
+  project            = var.project
+  environment        = var.environment
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  public_subnet_ids  = module.vpc.public_subnet_ids
+}

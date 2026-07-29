00e6f72af43c16f61f5837d645fa66ea57d3d7a1
qezman <holaryinka5050@gmail.com>
2026-07-29 13:16:12 +0100

    feat: install Kyverno via local chart tarball

--- environments/dev/kyverno.tf
+++ environments/dev/kyverno.tf
@@ -7,6 +7,9 @@ resource "kubernetes_namespace" "kyverno" {
 resource "helm_release" "kyverno" {
   name = "kyverno"
   #   repository = "https://kyverno.github.io/kyverno"
+
+  # chart pulled from local cache, not repo - helm repo add fails against
+# github.io-hosted chart repos in this environment
   chart     = "/home/qossim_05/.cache/helm/repository/kyverno-3.2.6.tgz"
   namespace = kubernetes_namespace.kyverno.metadata[0].name
   #   version    = "3.2.6"

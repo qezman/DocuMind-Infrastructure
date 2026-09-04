1dd82ff072e3b832bddbea43e85e930b471cb7d7
qezman <holaryinka5050@gmail.com>
2026-09-04 08:28:29 +0100

    no changes

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -8,7 +8,6 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.main.endpoint
 }
 
-
 output "cluster_ca_certificate" {
   description = "Base64 encoded certificate authority data"
   value       = aws_eks_cluster.main.certificate_authority[0].data
